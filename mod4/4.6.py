339496fd61ee88f14c43e2fd2aff4ef79fa412ad
iilai <145665821+iilai@users.noreply.github.com>
2025-08-28 10:36:44 +0300

    Update 4.6.py

--- mod4/4.6.py
+++ mod4/4.6.py
@@ -3,10 +3,10 @@ import random
 N = int(input("Kuinka monta pistettä arvotaan? "))
 
 kerrat = 0
-ympyrassa = 0
+ympyrassan = 0
 while kerrat < N:
     if random.uniform(-1,1)**2 + random.uniform(-1,1)**2 < 1:
-        ympyrassa += 1
+        ympyrassan += 1
     kerrat += 1
 
-print("Arvio π ≈", 4 * ympyrassa / N)
+print("π ≈", 4 * ympyrassan / N)
