2108a47d99f7072bd6d69edc6d7c687dfcd41054
iilai <eeliraustin@gmail.com>
2025-10-29 11:33:00 +0200

    1

---
+++ mod12/12.2.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+hakusana = input("Anna paikkakunnan nimi, niin annan säätilan ja lämpötilan celsius asteina: ")
+
+apiavain = "fbedcbee6adc99715e0302d1a3c10a15"
+
+pyynto = f"https://api.openweathermap.org/data/2.5/weather?q={hakusana}&appid={apiavain}&units=metric&lang=fi"
+
+vastaus = requests.get(pyynto).json()
+
+if vastaus['cod'] == 200:
+    print(f"Sää paikassa {vastaus['name']}: {vastaus['weather'][0]['description']}")
+    print(f"Lämpötila: {vastaus['main']['temp']} celsius astetta.")
+else:
+    print("Haku ei onnistunut.")
